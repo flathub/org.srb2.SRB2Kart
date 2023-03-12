755d1c7c8830dc1631525b7d16963e42b2aa2afe
Lonsfor <Lonsfor@users.noreply.github.com>
2023-03-12 19:20:39 -0300

    Add gamescope extension

--- srb2kart.sh
+++ srb2kart.sh
@@ -3,5 +3,12 @@
 for i in {0..9}; do
 	test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
+
+export PATH="/app/utils/gamescope/bin:$PATH"
 export SRB2WADDIR=/app/extra
-srb2kart "$@"
+
+if [[ -n $(command -v gamescope) && -n "$GAMESCOPE_ENABLE" ]]; then
+	echo "$GAMESCOPE_ARGS" -- srb2kart "$@" | xargs gamescope
+else
+	srb2kart "$@"
+fi
